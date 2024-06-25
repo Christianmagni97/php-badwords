48e537cbca86cbbee3f1d7cc1489b6d685f60e47
Christianmagni97 <christianmagni28@gmail.com>
2024-06-25 17:40:19 +0200

    prima push

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Potion of Geralt</h1>
+    <?php
+    $name = "Cat is one of the potions witchers use most often. After imbibing it, the witcher is able to see in total darkness and will avoid being surprised by any nocturnal creatures. The potion allows its user to see both living creatures and those made of inanimate matter, even through walls.
+    Cat slightly disrupts perception, however, as a result of which its user deals less damage to monsters. The Rook potion can be used to alleviate this side effect.
+    Potion causes minor intoxication when imbibed.
+    The Cat potion is recommended before entering a cave or a crypt, and before a midnight stroll through dangerous terrain.";
+    echo $name;
+
+    
+    ?>
+    
+</body>
+</html>
